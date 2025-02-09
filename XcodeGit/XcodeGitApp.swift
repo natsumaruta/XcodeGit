dc73e9cc942ae383e25808c1d3e448fec413ae6e
natsumaruta <nabeta0915@gmail.com>
2025-02-09 12:01:14 +0900

    Initial Commit

---
+++ XcodeGit/XcodeGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeGitApp.swift
+//  XcodeGit
+//
+//  Created by 米澤菜摘子 on 2025/02/09.
+//
+
+import SwiftUI
+
+@main
+struct XcodeGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
